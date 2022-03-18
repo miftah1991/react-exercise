d303c7b5762bff35adc5d8a63f3e1a0019350db0
miftah1991 <miftah.amin1991@gmail.com>
2022-03-19 00:08:03 +0430

    CampSite excercise

---
+++ src/modules/CampSite.js
@@ -0,0 +1,24 @@
+class CampSite extends React.Component {
+    constructor(props) {
+      super(props);
+    }
+    render() {
+      return (
+        <div>
+          <Camper/>
+        </div>
+      );
+    }
+  };
+  // Change code below this line
+  const Camper = (props) =>{
+    return(
+      <p>{props.name}</p>
+    )
+  };
+  Camper.defaultProps = {
+    name: "CamperBot"
+  };
+  Camper.propTypes = {
+    name: PropTypes.string.isRequired
+  };
